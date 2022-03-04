a38a25e87f358c0ea8475051eaef19c3288f835f
Albert Pangestu <stamina.left@gmail.com>
2022-03-04 20:43:54 +0700

    Created Presenter protocol

---
+++ PomodoroClock/StartPomodoroTimerPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  StartPomodoroTimerPresenter.swift
+//  PomodoroClock
+//
+//  Created by Albert Pangestu on 04/03/22.
+//
+
+import Foundation
+
+protocol StartPomodoroTimerPresenterOutput {
+    func displayWorkingTimer(response workingTimer: Int)
+}
